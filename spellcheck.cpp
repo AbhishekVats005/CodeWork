88f9f8c29651f7d1062572053a070ffd30bed72b
Abhishek Vats <114924682+AbhishekVats005@users.noreply.github.com>
2022-10-03 18:57:37 +0530

    Codechef

---
+++ spellcheck.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void atom(){
+    string lc;
+    cin>>lc;
+
+    if(lc == 'Timur' || lc == 'miurT' || lc == 'Trumi' || lc == 'mriTu'){
+        cout<<"YES"<<endl;
+        return;
+    }
+    else{
+        cout<<"NO"<<endl;
+        return;
+    }
+}
+int main(){
+    int sac=1;
+    cin>>sac;
+    for(int i=0;i<=sac;i++){
+        atom();
+    }
+    return 0;
+}
